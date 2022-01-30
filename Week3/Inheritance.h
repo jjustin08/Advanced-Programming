770463677dfe201dd8785faee84f64d9196d9f6e
jjustin08 <justin_noah17@yahoo.ca>
2022-01-30 15:59:33 -0500

    [ADD] Inheritance.h Initial person class defined

---
+++ Week3/Inheritance.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum class Discipline {GAME_DEVELOPER, SOFTWARE_ENGINEER, ANIMATOR};
+enum class Classification {FRESHMAN, SOPHMORE, JUNIOR, SENIOR};
+
+// base class
+class Person
+{
+private:
+	string name;
+
+
+public:
+	Person()
+	{
+		setName("");
+	}
+	Person(const string& name)
+	{
+		setName(name);
+	}
+	void setName(const string& name)
+	{
+		this->name = name;
+	}
+	string getName()
+	{
+		return name;
+	}
+
+
+};
