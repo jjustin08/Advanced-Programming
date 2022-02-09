6e43667a8cd8a263d9f781935b4039674b6a71fe
jjustin08 <justin_noah17@yahoo.ca>
2022-02-09 13:10:22 -0500

    [ADD] Initial function template example

---
+++ Week5/Example1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+template <class T>
+T square(T number)
+{
+	return number * number;
+}
+
+
+int main()
+{
+	int iValue;
+	double dValue;
+	long lValue;
+
+	// Getting a value and squaring it
+	cout << "Enter an integer: ";
+	cin >> iValue;
+
+	// The compilar will create an "int square(int)" when the call occurs.
+	cout << "The square is " << square(iValue) << endl;
+
+	cout << "Enter an double: ";
+	cin >> dValue;
+	cout << "The square is " << square(dValue) << endl;
+
+	cout << "Enter an long: ";
+	cin >> lValue;
+	cout << "The square is " << square(lValue) << endl;
+
+	return 0;
+}
