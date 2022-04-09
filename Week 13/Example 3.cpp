f9282160ca3b0060f9b54ce7bebf1f4d85a994d8
jjustin08 <justin_noah17@yahoo.ca>
2022-04-09 19:34:23 -0400

    [ADD] example 3 - bad_alloc

--- Week 13/Example 3.cpp
+++ Week 13/Example 3.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <new>
 using namespace std;
+
+// bad_allo occurs when you cannot allocate memory
 
 int main()
 {
+	int* p;
+	try
+	{
+		p = new int[2];
+		p[0] = 10;
+		p[1] = 20;
+		throw bad_alloc();
+	}
+	catch (bad_alloc b)
+	{
+		cout << b.what();
+		exit(1);
+	}
 
+	cout << p[0] << " " << p[1] << endl;
 
 	return 0;
 }
