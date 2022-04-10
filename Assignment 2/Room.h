f751e6ef2e41dc6b39b02e16ed830caf2d9f9dc8
jjustin08 <justin_noah17@yahoo.ca>
2022-04-10 19:18:09 -0400

    [ADD] room class

--- Assignment 2/Room.h
+++ Assignment 2/Room.h
@@ -1,5 +1,20 @@
 #pragma once
-class Room
+#include <iostream>
+#include <string>
+#include "Entity.h"
+#include "Objects.h"
+#include "Item.h"
+using namespace std;
+class Room : public Entity
 {
+private:
+	vector<Room*> connectingRooms;
+	vector<Objects*> objects;
+	vector<Item*> items;
+public:
+	Room();
+	~Room();
+	void loadFile(string fileName);
+
 };
 
